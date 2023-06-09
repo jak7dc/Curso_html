cc9b668b7031f6978eb69d0f5adab7f656c0da57
yevgeny <diacs.ingenieria@gmail.com>
2023-06-09 15:57:49 -0500

    curso de html primer ejercicio

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
-console.log('hola mundo')
+function viajar(destino) {
+  if (destino === 'Brasil') {
+    console.log('Gire a la izquierda');
+  } else if (destino === 'Argentina') {
+    console.log('Gire a la derecha');
+  } else {
+    console.log('Nos perdimos'); ''
+  }
+};
+viajar('Brasil');
+viajar('Argentia');
 
 
-// // console.log('ctrl e + ctrl w, add coment')
+for (let i = 0; i < 5; i++) {
+  suma = suma + i;
+  console.log('Variable de iteración: ', i);
+}
+console.log('Variable suma: ', suma);
+
+while (suma < 3) {
+  suma = suma + 1;
+  console.log(suma);
+};
+
+var elementoDelArray = listaDeCompras[1];
+console.log(elementoDelArray);
+
+
+
+// PUSH y UNSHIFT
+var colores = ['Amarillo', 'Azul'];
+colores.push('Rojo');
+colores.unshift('Verde');
+
+console.log(colores);
+
+// POP y SHIFT
+colores.shift();
+colores.pop();
+
+console.log(colores)
+
+
+// FOREACH
+var numeros = [1, 2, 3, 4];
+numeros.forEach((num) => { console.log(num) })
+
+// MAP
+var numeros = [1, 2, 3, 4];
+var masUno = numeros.map((num) => { return num + 1 });
+
+console.log(masUno);
+
+// esta es un commit de prueba para ver que hace el git restore
